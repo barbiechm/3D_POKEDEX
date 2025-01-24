0bca48ca047504b30e1c66ce3b172a2d7b4c705e
Barbara Chacin <barbaraasucar@gmail.com>
2025-01-24 15:58:09 -0400

    orbit controls added and model default position fixed

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import * as THREE from 'three';
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({alpha: true});
 
-const renderer = new THREE.WebGLRenderer();
 renderer.setSize(500, 500);
+renderer.setClearColor(0x000000, 0);
 document.getElementById('three-container').appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1 );
@@ -15,23 +17,39 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 //scene.add( cube );
 
-camera.position.z = 2;
+const controls = new OrbitControls( camera, renderer.domElement );
 
 
-camera.position.set(1, 1, 1);
+//camera.position.set(0, 0.2, 2);
+controls.update();
 
   // Apuntar la cámara hacia el centro
-  camera.lookAt(0, 0, 0);
+ // camera.lookAt(2, 0, 0);
+
+camera.fov = 50; // Ajusta este valor según sea necesario
+camera.updateProjectionMatrix(); 
 
 
-const light = new THREE.AmbientLight(0xffffff, 2);
+const light = new THREE.AmbientLight(0xffffff, 2.5);
         scene.add(light);
 
+		//GLTF
 const loader = new GLTFLoader();
 
 loader.load( '/VAPOREON.gltf', function ( gltf ) {
+    const model = gltf.scene;
+	scene.add( model );
 
-	scene.add( gltf.scene );
+	model.position.y = -0.4;
+    const box = new THREE.Box3().setFromObject(model);
+    
+
+    const center = box.getCenter(new THREE.Vector3());
+	
+	
+	
+	camera.position.set(center.x - 0.5, center.y, center.z + 1.4); // Ajusta la altura y distancia según sea necesario
+    camera.lookAt(center);
 
 }, undefined, function ( error ) {
 
@@ -41,7 +59,7 @@ loader.load( '/VAPOREON.gltf', function ( gltf ) {
 
 function animate() {
 	requestAnimationFrame(animate);
-
+    controls.update();
 	renderer.render( scene, camera );
 
 }
