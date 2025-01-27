935c52f130ab81c0373938adec35dd054b59e922
Barbara Chacin <barbaraasucar@gmail.com>
2025-01-26 22:32:10 -0400

    cdn deploy test #1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.172.0/build/three.module.js';
 
-import { GLTFLoader } from "/node_modules/three/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Configuración de la escena
 const scene = new THREE.Scene();
